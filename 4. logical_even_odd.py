ef0db5c922d1d484c89fae09daa91a65a74dbcdb
Rushabh Halmare <42460595+rushabhhalmare09@users.noreply.github.com>
2023-03-30 23:38:05 +0530

    Basics

---
+++ 4. logical_even_odd.py
@@ -0,0 +1,8 @@
+a1 = int(input("Enter number: - "))
+
+a2 = int(input("Enter number: - "))
+
+if a1 > 10 and a2 > 10:
+    print("both greater than 10")
+else:
+    print("Less than 10")
---
+++ 6. while_n_natural_no.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter a number : - "))
+count = 1
+
+while count <= num1:
+    print(count)
+    count = count + 1
