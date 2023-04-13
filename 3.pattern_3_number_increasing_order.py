99181092c692413798b3721e688fe4ebe2cb151f
Rushabh Halmare <42460595+rushabhhalmare09@users.noreply.github.com>
2023-04-13 22:40:49 +0530

    basics

---
+++ 3.pattern_3_number_increasing_order.py
@@ -0,0 +1,35 @@
+
+##Check why it is printing None in result after printing correct output
+##NNumbers in decreasing order
+
+
+def pattern(num, row, column):
+    i = row
+    while i <= num:
+        j = column
+        while j <= num:
+            print(j, end=" ")
+            j = j + 1
+        print()
+        i = i + 1
+
+num1= int(input("Enter a number: - "))
+row= int(input("Enter a row number: - "))
+column= int(input("Enter a column number: - "))
+
+# row and column put it 1 only 
+func_obj = pattern(num1,row, column)
+print(func_obj)
+
+
+
+## Without Function        
+# # num = int(input("Enter a number: -"))
+# # i = 1
+# while i <= num:
+#     j = 1
+#     while j <= num:
+#         print(j, end=" ")
+#         j = j + 1
+# #   print()
+# #   i = i + 1
