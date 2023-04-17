bd01f41e145b48fe495bd02e51e6a4759ef87056
Rushabh Halmare <42460595+rushabhhalmare09@users.noreply.github.com>
2023-04-17 22:46:15 +0530

    basics

---
+++ pattern/1. pattern_1_star.py
@@ -0,0 +1,30 @@
+
+##Check why it is printing None in result after printing correct output
+##Star will be printed
+def pattern(num):
+    i = 1
+    while i <= num:
+        j = 1
+        while j <= num:
+            print("*", end=" ")
+            j = j + 1
+        print()
+        i = i + 1
+
+num1= int(input("Enter a number: - "))
+func_obj = pattern(num1)
+
+print(func_obj)
+
+
+
+## Without Function        
+# num = int(input("Enter a number: -"))
+# i = 1
+# while i <= num:
+#     j = 1
+#     while j <= num:
+#         print("*", end=" ")
+#         j = j + 1
+#     print()
+#     i = i + 1
