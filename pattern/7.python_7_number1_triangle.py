fc290abcd4fc1a386ccb35be4e07fa90bd46e568
Rushabh Halmare <42460595+rushabhhalmare09@users.noreply.github.com>
2023-04-21 01:01:38 +0530

    basics

---
+++ pattern/7.python_7_number1_triangle.py
@@ -0,0 +1,35 @@
+
+##Check why it is printing None in result after printing correct output
+##Number will be printed instead of *
+
+
+def pattern(num):
+    i = 1
+    while i <= num:
+        j = 1
+        while j <= i:
+            print(i, end=" ")
+            j = j + 1
+        print()
+        i = i + 1
+
+num1= int(input("Enter a number: - "))
+
+# row and column put it 1 only 
+func_obj = pattern(num1)
+
+print(func_obj)
+
+ß
+
+
+## Without Function        
+# num = int(input("Enter a number: -"))
+# i = 1
+# while i <= num:
+#     j = 1
+#     while j <= i:
+#         print("*", end=" ")
+#         j = j + 1
+#     print()
+#     i = i + 1
