476e7e70c64feafed2f394bdd34db4cea207195e
Rushabh Halmare <42460595+rushabhhalmare09@users.noreply.github.com>
2023-04-23 01:48:25 +0530

    basics

---
+++ pattern/10.python_10_number_3triangle_increasing.py
@@ -0,0 +1,37 @@
+
+##Check why it is printing None in result after printing correct output
+##Number will be printed instead of *
+
+
+def pattern(num):
+    i = 1
+    n = 1  # this will not reset n again, so the number won't repeat itself
+    while i <= num:
+        j = 1
+        while j <= i:
+            print(n, end=" ")
+            j = j + 1
+            n = n + 1
+        print()
+        i = i + 1
+
+num1= int(input("Enter a number: - "))
+
+# row and column put it 1 only 
+func_obj = pattern(num1)
+
+print(func_obj)
+
+
+
+
+## Without Function        
+# num = int(input("Enter a number: -"))
+# i = 1
+# while i <= num:
+#     j = 1
+#     while j <= i:
+#         print("*", end=" ")
+#         j = j + 1
+#     print()
+#     i = i + 1
